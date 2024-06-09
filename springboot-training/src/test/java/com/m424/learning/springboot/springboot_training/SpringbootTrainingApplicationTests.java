9e875fd8813338e187facb8db12b3c5cb6c76551
M424 <mys40651147@gmail.com>
2024-06-09 22:01:03 +0900

    create

---
+++ springboot-training/src/test/java/com/m424/learning/springboot/springboot_training/SpringbootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m424.learning.springboot.springboot_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
